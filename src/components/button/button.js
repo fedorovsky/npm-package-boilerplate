e13efd7bb9a83c7d16ba297e4a8257069771accd
fedorovskyi <anton.fedorovsky@gmail.com>
2025-06-22 21:44:13 +0200

    structure

--- src/components/button/button.js
+++
@@ -1,5 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import styles from './button.module.css';
-export const Button = () => {
-    return _jsx("button", { className: styles.button, children: "Click me!" });
-};
