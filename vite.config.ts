0605693103d52bccb33874a404ef1ea5332bea75
fedorovskyi <anton.fedorovsky@gmail.com>
2025-06-24 15:45:10 +0200

    added inject css

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
 import wyw from '@wyw-in-js/vite';
+import { libInjectCss } from 'vite-plugin-lib-inject-css';
 import path from 'path';
 
 export default defineConfig({
   plugins: [
+    libInjectCss(),
     dts(),
     wyw({
       include: ['src/**/*.{ts,tsx}'],
